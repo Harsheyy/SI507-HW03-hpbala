503997511d26de08ac2021f8e1ea2654f8ff87d4
vandegu <avandegu@umich.edu>
2018-09-19 10:26:57 -0400

    Added the answer functionality

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,16 @@
+from random import randint
+
 def askUser():
 	x = str(input("What is your question?"))
 	return x
 
 b = askUser()
+
+def answerUser():
+	x = randint(0,9)
+	
+	answers = ['Reply hazy, try again', 'Ask again later.','Better not tell you now.','Cannot predict now.',
+		   'Concentrate and ask again.',"Don't count on it.",'My reply is no.','My sources say no.',
+		   'Outlook not so good.','Very doubtful.']
+	
+	return answers[x]
