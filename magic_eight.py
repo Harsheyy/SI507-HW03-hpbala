4ca60fd472ba52dc7d42d191bb2f0c7b9dd3d66d
Harsh K Desai <harshkdesai109@yahoo.com>
2018-09-19 10:29:11 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -10,3 +10,5 @@ def answerUser():
 	x = randint(0,9)
 	list = ["It is decidedly so.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."]
 	return list[x]	
+
+c = answerUser()
