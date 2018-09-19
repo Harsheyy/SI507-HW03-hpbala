221a3711ded60fe708ef4a334aee8e76f0172492
Harsh K Desai <harshkdesai109@yahoo.com>
2018-09-19 10:30:24 -0400

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -4,11 +4,10 @@ def askUser():
 	x = str(input("What is your question?"))
 	return x
 
-b = askUser()
-
 def answerUser():
 	x = randint(0,9)
 	list = ["It is decidedly so.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes."]
-	return list[x]	
+	print(list[x])
 
-c = answerUser()
+b = askUser()
+answerUser()
